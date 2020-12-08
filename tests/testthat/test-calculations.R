43df11950a5b35a88cf3de646dd4e71f6dc5b413
acircleda <acornrevolution@yahoo.com>
2020-12-07 21:17:59 -0500

    Fixed spelling errors

--- tests/testthat/test-calculations.R
+++ tests/testthat/test-calculations.R
@@ -1,8 +1,13 @@
 test_that("airport_footprint() works", {
   tol = 0.1
   expect_equal(airport_footprint("LAX", "PUS"), 1882.79, tolerance = tol)
+  expect_equal(airport_footprint("LAX", "PUS", output="n20"), 9.336, tolerance = tol)
   expect_equal(airport_footprint("LAX", "PUS", "First"), 5767.621, tolerance = tol)
+
   expect_equal(airport_footprint("LAX", "PUS", "First", "ch4"), 0.1924946, tolerance = tol)
   expect_type(airport_footprint("LAX", "PUS"), "double")
   expect_error(airport_footprint("LAX"))
+  expect_equal(latlong_footprint(34.052235, -118.243683, 35.179554, 129.075638), 1881.589, tolerance = tol)
+  expect_type(latlong_footprint(34.052235, -118.243683, 35.179554, 129.075638), "double")
+  expect_error(latlong_footprint(34.052235, -118.243683, 35.179554))
 })
